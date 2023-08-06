7720a955fdcc08c0ddcdcc4c672dafd08712ea59
Mercy Shyu <mercy@wossom.com>
2023-08-06 23:32:26 +0200

    Added structure

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/detail">Detail</RouterLink>
       </nav>
     </div>
   </header>
